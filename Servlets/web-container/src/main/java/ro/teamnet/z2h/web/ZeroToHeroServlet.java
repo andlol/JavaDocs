facd67400e6695b9b07adeb96676d53fc526a0f4
andlol <andrei_ap_93@yahoo.com>
2015-05-04 18:02:04 +0300

    ServeletApp

--- Servlets/web-container/src/main/java/ro/teamnet/z2h/web/ZeroToHeroServlet.java
+++ Servlets/web-container/src/main/java/ro/teamnet/z2h/web/ZeroToHeroServlet.java
@@ -15,12 +15,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ZeroToHeroServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO de implementat
+        resp.setContentType("text/html");
+        String res=handleRequest(req);
+        PrintWriter out = resp.getWriter();
+        out.println(res);
+
+    }
+
+    private String handleRequest(HttpServletRequest req){
+        String x = req.getParameter("first");
+        String y = req.getParameter("last");
+        String response="Hello <b>"+x+" "+y+"</b>! Enjoy Zero To Hero!!!";
+
+        return response;
     }
 
 }
